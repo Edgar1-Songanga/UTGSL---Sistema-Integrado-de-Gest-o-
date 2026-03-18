31662d6b7a2241f695d1ce21f3e3123919631b22
Edgar1-Songanga <217579440+Edgar1-Songanga@users.noreply.github.com>
2026-03-18 10:10:15 +0100

    Update Dashboard.jsx

--- public/src/pages/Dashboard.jsx
+++ public/src/pages/Dashboard.jsx
@@ -1,1 +1,28 @@
+export default function Dashboard(){
+  return (
+    <div>
 
+      <h1 className="text-2xl font-semibold mb-6">Visão Geral</h1>
+
+      <div className="grid grid-cols-3 gap-6">
+
+        <div className="bg-white/5 p-6 rounded">
+          <p className="text-gray-400 text-sm">Clientes</p>
+          <h2 className="text-3xl font-bold">48</h2>
+        </div>
+
+        <div className="bg-white/5 p-6 rounded">
+          <p className="text-gray-400 text-sm">Reclamações</p>
+          <h2 className="text-3xl font-bold">12</h2>
+        </div>
+
+        <div className="bg-white/5 p-6 rounded">
+          <p className="text-gray-400 text-sm">Resolvidas</p>
+          <h2 className="text-3xl font-bold">30</h2>
+        </div>
+
+      </div>
+
+    </div>
+  );
+}
