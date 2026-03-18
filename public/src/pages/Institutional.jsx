dd3a666ce00ba1833151f4a90414672c475d8bed
Edgar1-Songanga <217579440+Edgar1-Songanga@users.noreply.github.com>
2026-03-18 10:25:58 +0100

    Update Institutional.jsx

--- public/src/pages/Institutional.jsx
+++ public/src/pages/Institutional.jsx
@@ -1,1 +1,8 @@
-
+export default function Institutional(){
+  return (
+    <div>
+      <h1 className="text-2xl mb-4">Institucional</h1>
+      <p>Missão, Visão e Valores...</p>
+    </div>
+  );
+}
