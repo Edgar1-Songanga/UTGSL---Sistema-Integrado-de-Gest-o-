92cabdd14bd3107a6eda7c86e1f1350b4cf0f399
Edgar1-Songanga <217579440+Edgar1-Songanga@users.noreply.github.com>
2026-03-18 10:32:58 +0100

    Update Button.jsx

--- public/src/components/ui/Button.jsx
+++ public/src/components/ui/Button.jsx
@@ -1,1 +1,10 @@
-
+export default function Button({ children, onClick }) {
+  return (
+    <button 
+      onClick={onClick}
+      className="bg-green-500 px-4 py-2 rounded hover:bg-green-600"
+    >
+      {children}
+    </button>
+  );
+}
