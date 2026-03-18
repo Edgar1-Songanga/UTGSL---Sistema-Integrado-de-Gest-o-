aec2ca9b74883c3b63f043ceceae4d49fec3a337
Edgar1-Songanga <217579440+Edgar1-Songanga@users.noreply.github.com>
2026-03-18 10:02:37 +0100

    Update App.jsx

--- public/src/App.jsx
+++ public/src/App.jsx
@@ -1,1 +1,23 @@
+import { useState } from "react";
+import Layout from "./components/layout/Layout";
+import Dashboard from "./pages/Dashboard";
+import Clients from "./pages/Clients";
+import Complaints from "./pages/Complaints";
 
+export default function App() {
+  const [page, setPage] = useState("dashboard");
+
+  const renderPage = () => {
+    switch(page){
+      case "clients": return <Clients />;
+      case "complaints": return <Complaints />;
+      default: return <Dashboard />;
+    }
+  };
+
+  return (
+    <Layout setPage={setPage}>
+      {renderPage()}
+    </Layout>
+  );
+}
