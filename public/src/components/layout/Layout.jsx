8e8fe70433cbf9d40f0359ff5b6c37040c773441
Edgar1-Songanga <217579440+Edgar1-Songanga@users.noreply.github.com>
2026-03-18 10:04:23 +0100

    Add Layout component with Sidebar and Topbar

--- public/src/components/layout/Layout.jsx
+++ public/src/components/layout/Layout.jsx
@@ -1,1 +1,16 @@
+import Sidebar from "./Sidebar";
+import Topbar from "./Topbar";
 
+export default function Layout({ children, setPage }) {
+  return (
+    <div className="flex h-screen">
+      <Sidebar setPage={setPage}/>
+      <div className="flex-1 flex flex-col">
+        <Topbar />
+        <div className="p-6 overflow-y-auto">
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+}
